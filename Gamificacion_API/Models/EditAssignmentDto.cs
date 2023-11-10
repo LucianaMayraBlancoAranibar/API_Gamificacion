5d7a21fe9cd5f19401f8accfab4eb19ca0c7a766
LucianaMayraBlancoAranibar <bal0029274@est.univalle.edu>
2023-11-09 23:04:15 -0400

    cambios_crudasignacion

---
+++ Gamificacion_API/Models/EditAssignmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Gamificacion_API.Models
+{
+    public class EditAssignmentDto
+    {
+        public int IdStudent { get; set; } // ID del estudiante, si necesitas cambiarlo
+        public int IdAchievement { get; set; } // ID del logro, si necesitas cambiarlo
+        public int Points { get; set; }
+    }
+}
