99f9dc461239f3c4518f2533568f045aa00441d4
Steven Gabriel Claros Tapia <88797832+StevenGCT@users.noreply.github.com>
2023-11-11 21:24:38 -0400

    StudentController listo

--- Gamificacion_API/Controllers/StudentsController.cs
+++ Gamificacion_API/Controllers/StudentsController.cs
@@ -20,144 +20,206 @@ namespace Gamificacion_API.Controllers
         {
             _context = context;
         }
-        [HttpGet("{id}/GetStudentRank")]
-        public async Task<ActionResult<StudentRankDto>> GetStudentRank(int id)
-        {
-            var student = await _context.Students
-                .Include(s => s.IdRankNavigation)
-                .FirstOrDefaultAsync(s => s.IdStudent == id);
 
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            var rankDto = new StudentRankDto
-            {
-                FirstName = student.FirstName,
-                LastName = student.LastName,
-                Score = student.Score,
-                RankName = student.IdRankNavigation?.NameRank,
-                SubRankName = student.IdRankNavigation?.NameSubrank,
-                ImagePath = student.IdRankNavigation?.ImagePath
-            };
-
-
-            return Ok(rankDto);
-        }
 
         // GET: api/Students
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
         {
-          if (_context.Students == null)
-          {
-              return NotFound();
-          }
-            return await _context.Students.ToListAsync();
+            try
+            {
+                var estudiantesUsuariosInfo = await _context.Students
+                    .Include(g => g.IdStudentNavigation)
+                    .Select(g => new StudentUsuario
+                    {
+                        IdStudent = g.IdStudent,
+                        FirstName = g.FirstName,
+                        LastName = g.LastName,
+                        Score = g.Score,
+                        Email = g.IdStudentNavigation.Email,
+                        Password = g.IdStudentNavigation.Password,
+                        Rol = g.IdStudentNavigation.Rol,
+                        IdRank = g.IdRank,
+                        IdAcademicUnity = g.IdStudentNavigation.IdAcademicUnity,
+                        IdCareer = (int)g.IdStudentNavigation.IdCareer,
+                    })
+                    .ToListAsync();
+
+                return Ok(estudiantesUsuariosInfo);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno del servidor: " + ex.Message);
+            }
         }
 
         // GET: api/Students/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Student>> GetStudent(int id)
         {
-          if (_context.Students == null)
-          {
-              return NotFound();
-          }
-            var student = await _context.Students.FindAsync(id);
-
-            if (student == null)
+            try
             {
-                return NotFound();
-            }
+                var estudianteUsuario = await _context.Students
+                    .Include(g => g.IdStudentNavigation)
+                    .Where(g => g.IdStudent == id)
+                    .Select(g => new StudentUsuario
+                    {
+                        IdStudent = g.IdStudent,
+                        FirstName = g.FirstName,
+                        LastName = g.LastName,
+                        Score = g.Score,
+                        Email = g.IdStudentNavigation.Email,
+                        Password = g.IdStudentNavigation.Password,
+                        Rol = g.IdStudentNavigation.Rol,
+                        IdRank = g.IdRank,
+                        IdAcademicUnity = g.IdStudentNavigation.IdAcademicUnity,
+                        IdCareer = (int)g.IdStudentNavigation.IdCareer,
+                    })
+                    .FirstOrDefaultAsync();
 
-            return student;
+                if (estudianteUsuario == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(estudianteUsuario);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno del servidor: " + ex.Message);
+            }
         }
 
         // PUT: api/Students/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutStudent(int id, Student student)
+        public async Task<IActionResult> UpdateStudentAndUser(int id, [FromBody] StudentUsuario updatedStudentUser)
         {
-            if (id != student.IdStudent)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(student).State = EntityState.Modified;
-
+            //var existingUser = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == updatedGestorUser.Email);
+            //if (existingUser != null)
+            //{
+            //    return BadRequest("El correo electrónico ya está registrado.");
+            //}
             try
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(id))
+                var estudiante = await _context.Students.FirstOrDefaultAsync(g => g.IdStudent == id);
+
+                if (estudiante == null)
                 {
                     return NotFound();
                 }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return NoContent();
+                var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.IdUsuario == id);
+
+                if (usuario == null)
+                {
+                    return NotFound();
+                }
+
+                estudiante.FirstName = updatedStudentUser.FirstName;
+                estudiante.LastName = updatedStudentUser.LastName;
+                estudiante.Score = updatedStudentUser.Score;
+                usuario.Email = updatedStudentUser.Email;
+                /*Password ?*/
+                usuario.Rol = updatedStudentUser.Rol;
+                usuario.IdAcademicUnity = updatedStudentUser.IdAcademicUnity;
+                usuario.IdCareer = updatedStudentUser.IdCareer;
+                usuario.IdAcademicUnity = updatedStudentUser.IdAcademicUnity;
+                usuario.IdCareer = updatedStudentUser.IdCareer;
+                /*
+                if (usuario.Password != updatedGestorUser.Password)
+                {
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(updatedGestorUser.Password);
+                    usuario.Password = hashedPassword;
+                }*/
+
+                await _context.SaveChangesAsync();
+
+                return Ok("Estudiante actualizado con éxito.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno del servidor: " + ex.Message);
+            }
         }
 
         // POST: api/Students
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Student>> PostStudent(Student student)
+        public async Task<IActionResult> PostStudent([FromBody] StudentUsuario gup)
         {
-          if (_context.Students == null)
-          {
-              return Problem("Entity set 'BdgamificacionContext.Students'  is null.");
-          }
-            _context.Students.Add(student);
-            try
+            var existingUser = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == gup.Email);
+            if (existingUser != null)
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (StudentExists(student.IdStudent))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
+                return BadRequest("El correo electrónico ya está registrado.");
             }
 
-            return CreatedAtAction("GetStudent", new { id = student.IdStudent }, student);
+            try
+            {
+                using (var transaction = _context.Database.BeginTransaction())
+                {
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(gup.Password);
+
+                    var user = new Usuario
+                    {
+                        Email = gup.Email,
+                        Rol = gup.Rol,
+                        Password = hashedPassword,
+                        IdAcademicUnity = gup.IdAcademicUnity,
+                        IdCareer = gup.IdCareer
+                    };
+
+                    var student = new Student
+                    {
+                        FirstName = gup.FirstName,
+                        LastName = gup.LastName,
+                        Score = gup.Score,
+                        IdRank = gup.IdRank,
+                        IdStudentNavigation = user
+                    };
+
+                    _context.Usuarios.Add(user);
+                    _context.Students.Add(student);
+
+                    await _context.SaveChangesAsync();
+
+                    transaction.Commit();
+
+                    return Ok("Estudiante y usuario creados con éxito.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Error interno del servidor: " + ex.Message);
+            }
         }
 
         // DELETE: api/Students/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteStudent(int id)
         {
-            if (_context.Students == null)
+            try
             {
-                return NotFound();
+                var student = await _context.Students.FirstOrDefaultAsync(g => g.IdStudent == id);
+                var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.IdUsuario == id);
+
+                if (student == null || usuario == null)
+                {
+                    return NotFound();
+                }
+
+                // Elimina el gestor de la base de datos
+                _context.Students.Remove(student);
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
-            var student = await _context.Students.FindAsync(id);
-            if (student == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                return StatusCode(500, "Error interno del servidor: " + ex.Message);
             }
-
-            _context.Students.Remove(student);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool StudentExists(int id)
-        {
-            return (_context.Students?.Any(e => e.IdStudent == id)).GetValueOrDefault();
         }
     }
 }
--- Gamificacion_API/Models/StudentUsuario.cs
+++ Gamificacion_API/Models/StudentUsuario.cs
@@ -15,7 +15,7 @@
 
         public byte Rol { get; set; }
 
-        public string Password { get; set; } = null!;
+        public string? Password { get; set; } = null!;
 
         public int IdCareer { get; set; }
 
